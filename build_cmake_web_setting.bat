084b8a111fbf7528fd221bf21e23e4f95c1e04ba
mage <wangm4a1@gmail.com>
2025-07-20 17:52:28 +0800

    fix file location

--- build_cmake_web_setting.bat
+++ build_cmake_web_setting.bat
@@ -6,10 +6,10 @@ set "SCRIPT_DIR=%~dp0"
 cd /d "%SCRIPT_DIR%emsdk"
 call emsdk_env.bat
 
-cd /d "%SCRIPT_DIR%"
+cd /d "%SCRIPT_DIR%raylib-game-template"
 
-if exist build_web rmdir /s /q build_web
+if exist ../build_web rmdir /s /q ../build_web
 
 echo.
 echo Configuring project for Web with Emscripten...
-"%SCRIPT_DIR%emsdk\upstream\emscripten\emcmake" "%SCRIPT_DIR%CMake\bin\cmake.exe" -S . -B build_web -G "Ninja" "-DCMAKE_MAKE_PROGRAM=%SCRIPT_DIR%ninja-bin\ninja.exe" -DPLATFORM=Web
+"%SCRIPT_DIR%emsdk\upstream\emscripten\emcmake" "%SCRIPT_DIR%CMake\bin\cmake.exe" -S . -B ../build_web -G "Ninja" "-DCMAKE_MAKE_PROGRAM=%SCRIPT_DIR%ninja-bin\ninja.exe" -DPLATFORM=Web
